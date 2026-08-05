5eeb7c58f865c569fd83804cb1ea557e3b4b5680
jack2468rwatpyau-hash <jack2468rwatpyau@gmail.com>
2026-08-05 22:58:21 +0630

    Create Review.js

---
+++ models/Review.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+  product_id: { type: Number, required: true, index: true },
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  order_id: { type: String, required: true },
+  rating: { type: Number, required: true, min: 1, max: 5 },
+  comment: { type: String, default: '', trim: true, maxlength: 500 },
+  images: { type: [String], default: [] },
+  created_at: { type: Date, default: Date.now },
+});
+
+reviewSchema.set('toJSON', {
+  transform: (doc, ret) => {
+    ret.id = ret._id.toString();
+    delete ret._id;
+    delete ret.__v;
+    return ret;
+  },
+});
+
+module.exports = mongoose.model('Review', reviewSchema);
